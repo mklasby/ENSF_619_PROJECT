c6b81b429efedda439dc4794c43733e1d56418d3
Mike Lasby <mklasby@ucalgary.ca>
2020-11-28 13:17:59 -0700

    fixed packages

--- TheatreModel/TheatreModel/Movie.java
+++ TheatreModel/TheatreModel/Movie.java
@@ -1,23 +1,28 @@
-package TheatreModel;
+package Model.TheatreModel;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import org.json.JSONObject;
 
-public class Movie {
+public class Movie extends JSONObject {
 	private String movieName;
 	private double moviePrice;
-	private boolean earlyAccess;             //what is the point in a movie object having this
+	private boolean earlyAccess; // what is the point in a movie object having this
 	private ArrayList<Theatre> theatreList;
-	
+
 	public Movie(String movieName, double moviePrice, boolean earlyAccess) {
 		setMovieName(movieName);
 		setMoviePrice(moviePrice);
 		setTheatreList(new ArrayList<Theatre>());
 	}
 
+	public Movie(ResultSet allMovies) {
+	}
+
 	private void setMoviePrice(double moviePrice) {
 		this.moviePrice = moviePrice;
 	}
-	
+
 	public double getMoviePrice() {
 		return moviePrice;
 	}
@@ -25,7 +30,7 @@ public class Movie {
 	public String getMovieName() {
 		return movieName;
 	}
-	
+
 	public void setMovieName(String movieName) {
 		this.movieName = movieName;
 	}
