d21986655b519f0f834bbc730b1bb4c0ae2fbebc
Patrick_Kwan <64910790+patrickjykwan@users.noreply.github.com>
2020-11-29 01:08:30 -0700

    Updated Seat and ShowTime with JSONObject along with some work on refundticket and registernewuser

--- TicketReservationApp/src/Model/UserModel/UserManager.java
+++ TicketReservationApp/src/Model/UserModel/UserManager.java
@@ -1,16 +1,34 @@
 package Model.UserModel;
 
+import java.sql.ResultSet;
+
 public class UserManager {
 	
 	User user;
+	boolean isRegisteredUser;
+	
+	
+	public UserManager() {
+		this.user = null;
+		isRegisteredUser = false;
+	}
 
-    public static void setUser(User user) {
-    	this.user = user;
+    public void loginUser(ResultSet resultSetUser) { // i changed it so it not static anymore
+    	
+    	// I am assuming that the user to give to me is not null
+    	// Mike said it would be a good idea to have user instantiate in User Manager == less coupling == more lonely
+    	this.user = new User(resultSetUser);
+    	this.isRegisteredUser = true;
+    }
+    
+    public void logoutUser() {
+    	this.user = null;
+    	this.isRegisteredUser = false;
     }
 
 	public boolean isRegistered() {
 		// TODO Auto-generated method stub
-		return false;
+		return this.isRegisteredUser;
 	}
 
 }
